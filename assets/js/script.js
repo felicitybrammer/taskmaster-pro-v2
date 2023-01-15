1a7ecb6d7584af120752c87fb3d2307f1ff6f6ff
Felicity Brammer <felicitybrammer@gmail.com>
2023-01-14 19:07:06 -0800

    styling additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,7 +133,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -161,16 +161,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(this).addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(this).removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(this).addClass("dropover-active");
+    $(this).addClass("bottom-trash-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(this).removeClass("dropover-active");
+    $(this).removeClass("bottom-trash-active");
   },
   update: function(event) {
     var tempArr = [];
@@ -239,6 +243,13 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 }
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
+
 // load tasks for the first time
 loadTasks();
 
